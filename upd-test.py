06314cc51f6237a2b6a7d0b5ef491c92818affb3
ya <yarlaz@gmail.com>
2018-08-13 07:27:30 +0200

    fix: add gpu support

--- upd-test.py
+++
@@ -1,8 +0,0 @@
-import socket
-port = 5000
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(("", port))
-print "waiting on port:", port
-while 1:
-    data, addr = s.recvfrom(1024)
-    print data
